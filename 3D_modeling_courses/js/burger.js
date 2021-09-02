9faaf70ccbd0a44bd2ff5df8274e1deb02f69a21
Дмитрий Карпенко <karpenkods@mail.ru>
2021-09-02 16:24:11 +0700

    add new files to 3D

---
+++ 3D_modeling_courses/js/burger.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+$('.header-container__burger').click(function() {
+        $('.header-container__burger').toggleClass('open-menu');
+        $('.header__list').toggleClass('open-menu');
+    });
+});
